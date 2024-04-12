e55085e51249a0894215aab16f6a101bfd1466f3
Laura Bailey <laurab@adaptsolutions.com.au>
2024-04-12 16:19:17 +1000

    Add blank project

---
+++ FlyoutBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlyoutBug;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ FlyoutBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FlyoutBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
