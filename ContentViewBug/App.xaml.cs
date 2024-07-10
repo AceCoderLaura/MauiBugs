77aa4e6c4beae68ec3ea03468cb70e37f1bb8415
Laura Bailey <laurab@adaptsolutions.com.au>
2024-07-10 14:10:44 +1000

    Add new bug project

---
+++ ContentViewBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ContentViewBug;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ ContentViewBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ContentViewBug;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
