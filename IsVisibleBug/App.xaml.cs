f683085e78650383f6c2b3eef1a8811623df206a
Laura Bailey <laurab@adaptsolutions.com.au>
2025-02-19 16:59:47 +1100

    Add blank project

---
+++ IsVisibleBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IsVisibleBug;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ IsVisibleBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IsVisibleBug;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
