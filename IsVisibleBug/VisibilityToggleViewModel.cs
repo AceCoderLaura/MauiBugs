8944898cfc61bb103c201fb4317299fc52fa5b3f
Laura Bailey <laurab@adaptsolutions.com.au>
2025-02-20 09:59:37 +1100

    Add demo

---
+++ IsVisibleBug/VisibilityToggleViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace IsVisibleBug;
+
+public class VisibilityToggleViewModel : INotifyPropertyChanged
+{
+    private bool _isVisible;
+
+    public VisibilityToggleViewModel()
+    {
+        ToggleVisibleCommand = new Command(ToggleVisibility);
+    }
+
+    private void ToggleVisibility()
+    {
+        IsVisible = !IsVisible;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public bool IsVisible
+    {
+        get => _isVisible;
+        set => SetField(ref _isVisible, value);
+    }
+
+    public ICommand ToggleVisibleCommand { get; }
+    public List<string> StringList { get; } = ["cat", "horse", "dog", "mouse"];
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
